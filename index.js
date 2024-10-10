ab3674e4d442fa71d5953e0da1e860716a67316b
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2024-10-10 15:23:01 +0200

    it works

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function displayHelloWorld(){
     document.getElementById("helloWorld").innerHTML = "Hello World";
 }
+
+let button = document.getElementById("helloWorldButton");
+button.addEventListener("click", displayHelloWorld);
