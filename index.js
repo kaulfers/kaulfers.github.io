c2f2e72db0c943214e69de9a5fee183f831d805e
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2024-10-11 10:38:28 +0200

    more error displaynad null checking

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ let userInformation = [("tim", "abc"), ("john", "abc")]
 
 function checkUserLogIn(){
     for (const userConst of userInformation){
+        if (user === null){
+            document.getElementById("welcomeMessage").innerHTML = "please enter your information"
+        }
         if (userConst[0] === userName && userConst[1] === password){
             user = userConst;
             document.getElementById("welcomeMessage").innerHTML = "Welcome" + user[0];
